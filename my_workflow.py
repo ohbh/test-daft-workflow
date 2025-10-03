5ea9c67822315f1f448447daabc7f0ad482674c9
Oliver Huang <oliverbhuang@gmail.com>
2025-10-03 15:39:31 -0700

    process items

--- my_workflow.py
+++ my_workflow.py
@@ -19,36 +19,11 @@ def mkdir() -> str:
 
 def my_workflow(name: str) -> dict:
     print(f"Hello, {name}!, starting a daft job")
-    df = daft.from_pydict(
-        {
-            "text": [
-                "Alice was beginning to get very tired of sitting by her sister on the bank.",
-                "So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid),",
-                "whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies,",
-                "when suddenly a White Rabbit with pink eyes ran close by her.",
-                "There was nothing so very remarkable in that;",
-                "nor did Alice think it so very much out of the way to hear the Rabbit say to itself, 'Oh dear! Oh dear! I shall be late!'",
-            ]
-        }
-    )
-
-    for i in range(300):
-        print(f"Newly Processing {i} of 300")
-        time.sleep(1)
-
-    results = {}
-    for i in range(3):
-        dest = mkdir()
-        df_with_emb = df.with_column("embedding", embed_text(df["text"]))
-        df_with_emb = df_with_emb.write_parquet(dest)
-        results[f"run_{i + 1}"] = dest
-
-    for i in range(300):
-        print(f"Processing again {i} of 300")
-        time.sleep(1)
 
     print(f"Thanks, {name}!, daft job completed")
-    return {"results": results}
+    processe_item(name, 300)
+
+    return {"results": "success"}
 
 def main(name: str):
     my_workflow(name)
@@ -59,3 +34,8 @@ if __name__ == "__main__":
     parser.add_argument("--name", type=str, required=True, help="Name to use in workflow")
     args = parser.parse_args()
     main(name=args.name)
+
+def processe_item(name: str, count: int):
+    for i in range(count):
+        print(f"Processing {name}'s item: {i} of {count}")
+        time.sleep(1)
