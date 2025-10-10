0e382782191d670c5e982ad323037dd25a0cf81c
Oliver Huang <oliverbhuang@gmail.com>
2025-10-09 20:48:54 -0700

    no args

--- my_workflow.py
+++ my_workflow.py
@@ -32,11 +32,7 @@ def main(name: str):
     my_workflow(name)
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--name", type=str, required=True, help="Name to use in workflow")
-    args = parser.parse_args()
-    main(name=args.name)
+    main(name="test")
 
 def processe_item(name: str, count: int):
     for i in range(count):
